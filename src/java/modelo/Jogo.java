16d519ede944a59e1bae8387d97bc7818c839f41
Fernanda Carvalho <borgesdecarvalho1998@gmail.com>
2016-04-18 16:34:43 -0300

    Mexendo no Jogo java

--- src/java/modelo/Jogo.java
+++ src/java/modelo/Jogo.java
@@ -1,6 +1,9 @@
 package modelo;
 
+import com.sun.istack.internal.logging.Logger;
+import dao.PerguntaDAO;
 import java.util.List;
+import java.util.logging.Level;
 
 public class Jogo {
     private List<Pergunta> perguntaList;
@@ -51,6 +54,43 @@ public class Jogo {
         Integer erro = 0;
         return erro;
     }
+    public void iniciar() 
+    {
+        try {
+            PerguntaDAO dao = new PerguntaDAO();
+        } catch (Exception ex) {
+            java.util.logging.Logger.getLogger(Jogo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
+    public boolean confirmar(String opcaomarcada)
+    {
+        // se ele acertar a pergunta...
+        if(opcaomarcada.equals(this.getPerguntaList().get(0).getCerta()))
+        {
+            // excluo a pergunta atual
+            this.getPerguntaList().remove(0);
+            // aumento a pontuacao dele
+            this.setPontuacao(this.getAcerto());
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public boolean pular()
+    {
+        if(this.pulos > 0) // se a quantidade de pulos for maior que 0, ele poderá pular
+        {
+            this.pulos = this.pulos - 1; // ele pula, e diminui a sua quantia disponível de pulos
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
     
-}
+    }
